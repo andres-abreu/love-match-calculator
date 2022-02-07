ad59bb54944fb45107be15518df105ec0a2b4d04
Alexandra Nunez <93490778+Zela7alex@users.noreply.github.com>
2022-02-07 11:42:40 -0500

    Turned spotify Json into an object

--- assets/spotify-API.js
+++ assets/spotify-API.js
@@ -1,45 +1,17 @@
+var myHeaders = new Headers();
+myHeaders.append("Content-Type", "application/json");
+var requestOptions = {
+  method: "get",
+  headers: myHeaders,
+  redirect: "follow",
 
+};
+var apiUrl = "https://v1.nocodeapi.com/zela7alex/spotify/xiGwjtGkQwDVRqNV/search?q=love&type=track&perPage=10"
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+fetch(apiUrl, requestOptions)
+  .then(response => response.json())
+  .then(result => console.log(result))
+  .catch(error => console.log('error', error));
 
 
 
