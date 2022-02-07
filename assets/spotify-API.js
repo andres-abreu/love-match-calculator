1248f14561103beb505e2945ee8a8449a400c440
Alexandra Nunez <93490778+Zela7alex@users.noreply.github.com>
2022-02-07 13:02:27 -0500

    Added spotify I hate love api

--- assets/spotify-API.js
+++ assets/spotify-API.js
@@ -1,3 +1,6 @@
+
+// COMPATIBLE Spotify API
+
 var myHeaders = new Headers();
 myHeaders.append("Content-Type", "application/json");
 var requestOptions = {
@@ -6,15 +9,31 @@ var requestOptions = {
   redirect: "follow",
 
 };
-var apiUrl = "https://v1.nocodeapi.com/zela7alex/spotify/xiGwjtGkQwDVRqNV/search?q=love&type=track&perPage=10"
+var apiLoveUrl = "https://v1.nocodeapi.com/zela7alex/spotify/xiGwjtGkQwDVRqNV/search?q=love&type=track&perPage=10"
 
-fetch(apiUrl, requestOptions)
+fetch(apiLoveUrl, requestOptions)
   .then(response => response.json())
   .then(result => console.log(result))
   .catch(error => console.log('error', error));
 
 
 
+// INCOMPATIBLE Spotify API
+
+var myHeaders = new Headers();
+myHeaders.append("Content-Type", "application/json");
+var requestOptions = {
+  method: "get",
+  headers: myHeaders,
+  redirect: "follow",
+
+};
+
+var apiHateUrl = "https://v1.nocodeapi.com/zela7alex/spotify/xiGwjtGkQwDVRqNV/search?q=I hate love&type=track&perPage=10"
+fetch(apiHateUrl, requestOptions)
+  .then(response => response.json())
+  .then(result => console.log(result))
+  .catch(error => console.log('error', error));
 
 
 
