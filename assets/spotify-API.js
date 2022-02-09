f6529d0dec484edd9db75dd564f348dc73b80620
Alexandra Nunez <93490778+Zela7alex@users.noreply.github.com>
2022-02-09 17:59:51 -0500

    Re-did href links to be in javascript

--- assets/spotify-API.js
+++ assets/spotify-API.js
@@ -2,18 +2,22 @@
 var spotifyImg1 = document.querySelector("#spotify-img-1")
 var spotifyTitle1 = document.querySelector("#spotify-title-1")
 var spotifyArtistname1 = document.querySelector("#spotify-artist-name-1")
+var spotifyLink1 = document.querySelector(".spotify-link-1")
 
 var spotifyImg2 = document.querySelector("#spotify-img-2")
 var spotifyTitle2 = document.querySelector("#spotify-title-2")
 var spotifyArtistname2 = document.querySelector("#spotify-artist-name-2")
+var spotifyLink2 = document.querySelector(".spotify-link-2")
 
 var spotifyImg3 = document.querySelector("#spotify-img-3")
 var spotifyTitle3 = document.querySelector("#spotify-title-3")
 var spotifyArtistname3 = document.querySelector("#spotify-artist-name-3")
+var spotifyLink3 = document.querySelector(".spotify-link-3")
 
 var spotifyImg4 = document.querySelector("#spotify-img-4")
 var spotifyTitle4 = document.querySelector("#spotify-title-4")
 var spotifyArtistname4 = document.querySelector("#spotify-artist-name-4")
+var spotifyLink4 = document.querySelector(".spotify-link-4")
 
 
 
@@ -33,18 +37,22 @@ fetch("https://v1.nocodeapi.com/andresabreu/spotify/jRyLOewVmZIgHgYl/search?q=lo
         spotifyTitle1.textContent = result.albums.items[0].name;
         spotifyImg1.setAttribute("src", result.albums.items[0].images[2].url);
         spotifyArtistname1.textContent = result.albums.items[0].artists[0].name;
+        spotifyLink1.setAttribute("href", result.albums.items[1].href)
 
         spotifyTitle2.textContent = result.albums.items[1].name;
         spotifyImg2.setAttribute("src", result.albums.items[1].images[2].url);
         spotifyArtistname2.textContent = result.albums.items[1].artists[0].name;
+        spotifyLink2.setAttribute("href", result.albums.items[2].href)
 
         spotifyTitle3.textContent = result.albums.items[3].name;
         spotifyImg3.setAttribute("src", result.albums.items[3].images[2].url);
         spotifyArtistname3.textContent = result.albums.items[3].artists[0].name;
+        spotifyLink3.setAttribute("href", result.albums.items[3].href)
 
         spotifyTitle4.textContent = result.albums.items[4].name;
         spotifyImg4.setAttribute("src", result.albums.items[4].images[2].url);
         spotifyArtistname4.textContent = result.albums.items[4].artists[0].name;
+        spotifyLink4.setAttribute("href", result.albums.items[4].href)
 
     })
     .catch(error => console.log('error', error));
@@ -90,24 +98,27 @@ fetch("https://v1.nocodeapi.com/andresabreu/spotify/jRyLOewVmZIgHgYl/search?q=I 
         spotifyTitle01.textContent = result.albums.items[0].name;
         spotifyImg01.setAttribute("src", result.albums.items[0].images[2].url);
         spotifyArtistname01.textContent = result.albums.items[0].artists[0].name;
-        spotifyLink01.setAttribute("href", result.albums.items)
+        spotifyLink01.setAttribute("href", result.albums.items[0].href)
 
         spotifyTitle02.textContent = result.albums.items[6].name;
         spotifyImg02.setAttribute("src", result.albums.items[1].images[2].url);
         spotifyArtistname02.textContent = result.albums.items[1].artists[0].name;
+        spotifyLink02.setAttribute("href", result.albums.items[1].href)
 
         spotifyTitle03.textContent = result.albums.items[7].name;
         spotifyImg03.setAttribute("src", result.albums.items[2].images[2].url);
         spotifyArtistname03.textContent = result.albums.items[2].artists[0].name;
+        spotifyLink03.setAttribute("href", result.albums.items[2].href)
 
         spotifyTitle04.textContent = result.albums.items[8].name;
         spotifyImg04.setAttribute("src", result.albums.items[3].images[2].url);
-        spotifyArtistname4.textContent = result.albums.items[3].artists[0].name;
+        spotifyArtistname04.textContent = result.albums.items[3].artists[0].name;
+        spotifyLink04.setAttribute("href", result.albums.items[3].href)
     })
     .catch(error => console.log('error', error));
 
 
-    //----------- Display Spotify Playlist on Button click-----------
+//----------- Display Spotify Playlist on Button click-----------
 
 $(document).ready(function () {
     $(".collection").hide();
