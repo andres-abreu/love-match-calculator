81d0f004144a649e91759d8f2af57e953c183839
A. Dorsey <ashadorsey@gmail.com>
2022-02-08 22:25:48 -0500

    fetchs pexels api photos

--- assets/pexels.js
+++ assets/pexels.js
@@ -1,56 +1,17 @@
+fetch("https://api.pexels.com/v1/search?query=love",{
+  headers: {
+    Authorization: "563492ad6f917000010000012696f567e48a4f6395dea73b9b1ad62f"
+  }
+})
+   .then(resp => {
+     return resp.json()
+   })
+   .then(data => {
+     console.log(data)
+   
+    })
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    
 
 
 //------------Button for Pexels--------------
